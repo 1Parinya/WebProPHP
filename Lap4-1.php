67584ee3a58c455fee2efbfbcf7313bba3fd4e83
1pariny <620602160102@fitm.kmutnb.ac.th>
2021-01-13 11:07:37 +0700

    credate Lap4-1.php

---
+++ Lap4-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+                $x = 100;
+                $y = "200";
+                $z = '300';
+                $add = $x + $y;
+                $sub = $z - $x;
+                $cuu = $y * $z;
+                $han = $cuu / 300;
+                $message = "Hello World";
+               
+                echo "ผลบวก 100+200 =".$add; echo "<br />";
+                echo "ผลลบ 300-100 =".$sub; echo "<br />";
+
+                echo "ผลคูณ 300 * 200 =".$cuu; echo "<br />";
+                echo "ผลหาร 600 / 300 =".$han; echo "<br />";
+                echo $message; echo "<br />";
+        ?>
+</body>
+</html>
---
+++ testfor.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST LOOP FOR</title>
+</head>
+<body>
+    <center>
+    <?php
+      for($i=1;$i<=7;$i++){
+         //echo ($i. " ");
+         echo("<font size=\"".$i."\">Welcome to PHP world</font><br />");
+         // echo("<font size=\"$i\">Welcome to PHP world</font><br />");
+        }
+    ?>
+    </center>
+
+
+</body>
+</html>
