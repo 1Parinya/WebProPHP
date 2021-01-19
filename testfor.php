51da4d38556af782d730efb06a62df9bdbeb43b5
1Parinya <6206021610102@fitm.kmutnb.ac.th>
2021-01-19 22:00:08 +0700

    update testfor.php

--- testfor.php
+++ testfor.php
@@ -8,7 +8,7 @@
 <body>
     <center>
     <?php
-      for($i=1;$i<=7;$i++){
+      for($i=1;$i<=8;$i++){
          //echo ($i. " ");
          echo("<font size=\"".$i."\">Welcome to PHP world</font><br />");
          // echo("<font size=\"$i\">Welcome to PHP world</font><br />");
