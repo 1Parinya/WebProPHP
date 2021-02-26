0896e21abc12f72dbdab4dd8da5c44272eed041d
1Parinya <6206021610102@fitm.kmutnb.ac.th>
2021-02-26 12:40:04 +0700

    update

--- LAP/L9/L9-2modifly.php
+++ LAP/L9/L9-2modifly.php
@@ -13,76 +13,75 @@
             <input type="submit" value="ตกลง"><br><br>
 
             <?php
-    error_reporting( error_reporting() & ~E_NOTICE );
-        $nstu = $_GET["ns"];
-        //$MaxStudent = 10;
-        $score = array();
+            error_reporting( error_reporting() & ~E_NOTICE );
+            $MaxStudent = $_GET["ns"];
+            $score = array();
+            
+            for($n = 0 ; $n < $MaxStudent; $n++) {
+            $score[$n] = rand(0, 100);
+            }
+            echo '<center><font size="4" color="blue"> Grade Report </font></center>';
+            echo '<table border="1" align="center">';
+            echo '<tr><td align="center" width="90">Student No.</td>';
+            echo '<td align="center" width="90">Score</td>';
+            echo '<td align="center" width="90">Geade</td></tr>';
+
+            for($n= 0 ; $n < $MaxStudent; $n++) {
+            echo '<tr><td align="center" width="90">' . ($n+1 ) . '</td>';
+            echo '<td align="center" width="90">' . $score[$n] . '</td>';
+            echo '<td align="center" width="90">'; 
+            calgrade($score[$n]);
+            echo '</td></tr>';
+            }
+            echo '<tr><td colspan="3" align="center"> Average Score : ';
+            echo average( $score, $MaxStudent);
+            
+
+            echo '<tr><td colspan="3" align="center">  student max Score :'.max($score).'';
+
+            echo '<tr><td colspan="3" align="center">  student min Score : '.min($score).'';
+            echo '</td></tr></table>';
+
+
+
+            function average($data, $max) {
+                $total = 0;
+                for($n = 0 ; $n < $max; $n++) {
+                $total += $data[$n];
+                }
+                return( $total / $max );
+                }
+
+            function calgrade($score){
+                if($score <= 49){
+                    echo("F");
+                }
+                else if($score <= 54){
+                    echo("D");
+                }
+                else if($score <= 59){
+                    echo("D+");
+                }
+                else if($score <= 64){
+                    echo("C");
+                }
+                else if($score <= 69){
+                    echo("C+");
+                }
+                else if($score <= 74){
+                    echo("B");
+                }
+                else if($score <= 79){
+                    echo("B+");
+                }
+                else if($score <=100 ){
+                    echo("A");
+                }
+            }
+
         
-        for($n = 0 ; $n < $nstu; $n++) {
-        $score[$n] = rand(0, 100);
-        }
-        echo '<center><font size="4" color="blue"> Grade Report </font></center>';
-        echo '<table border="1" align="center">';
-        echo '<tr><td align="center" width="90">Student No.</td>';
-        echo '<td align="center" width="90">Score</td>';
-        echo '<td align="center" width="90">Geade</td></tr>';
 
-        for($n= 0 ; $n < $nstu; $n++) {
-        echo '<tr><td align="center" width="90">' . ($n+1 ) . '</td>';
-        echo '<td align="center" width="90">' . $score[$n] . '</td>';
-        echo '<td align="center" width="90">'; 
-        calgrade($score[$n]);
-        echo '</td></tr>';
-        }
-        echo '<tr><td colspan="3" align="center"> Average Score : ';
-        echo average( $score, $nstu);
-        
-
-        echo '<tr><td colspan="3" align="center">  student max Score :'.max($score).'';
-
-        echo '<tr><td colspan="3" align="center">  student min Score : '.min($score).'';
-        echo '</td></tr></table>';
-
-
-
-        function average($data, $max) {
-            $total = 0;
-            for($n = 0 ; $n < $max; $n++) {
-            $total += $data[$n];
-            }
-            return( $total / $max );
-            }
-
-        function calgrade($score){
-            if($score <= 49){
-                echo("F");
-            }
-            else if($score <= 54){
-                echo("D");
-            }
-            else if($score <= 59){
-                echo("D+");
-            }
-            else if($score <= 64){
-                echo("C");
-            }
-            else if($score <= 69){
-                echo("C+");
-            }
-            else if($score <= 74){
-                echo("B");
-            }
-            else if($score <= 79){
-                echo("B+");
-            }
-            else if($score <=100 ){
-                echo("A");
-            }
-        }
-
-       
-
-    ?>
+        ?>
         </form>
     </center>
 </body>
